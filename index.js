a68258cc79b1c2c1f8403768bd32fd21b7d51e15
Jess <jessicatanhc@gmail.com>
2020-01-09 16:22:33 +0800

    print rgb values

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-function hexToRgb(hex) {
-  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result ? {
-    r: parseInt(result[1], 16),
-    g: parseInt(result[2], 16),
-    b: parseInt(result[3], 16)
-  } : null;
+var convertType = process.argv[2];
+var hexValue = process.argv[3];
+var hex = (hexValue)=> {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hexValue);
+  return result ?
+    "rgb( " + parseInt(result[1], 16)
+    +"," + parseInt(result[2], 16) +","+ parseInt(result[3], 16) + " )": null;
+}
+
+if(convertType === "hex")
+{
+    console.log(hex(hexValue));
 }
