65c9fd50caee851aeb83344831d54ed570bf3cfe
Jess <jessicatanhc@gmail.com>
2020-01-09 17:34:12 +0800

    Add HSL

--- convertHex.js
+++ convertHex.js
@@ -1,5 +1,56 @@
 module.exports.hexToRgb = (hex) => {
   var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
   return result ?
-    "rgb( "+parseInt(result[1], 16)+" "+parseInt(result[2], 16)+" "+ parseInt(result[3], 16) + ")" : null;
+    "rgb( "+parseInt(result[1], 16)+" "+parseInt(result[2], 16)+" "+ parseInt(result[3], 16) + " )" : null;
+}
+
+module.exports.hexToHSL = (hex) => {
+   var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+   var r = parseInt(result[1], 16);
+   var g = parseInt(result[2], 16);
+   var b = parseInt(result[3], 16);
+  // Make r, g, and b fractions of 1
+  r /= 255;
+  g /= 255;
+  b /= 255;
+
+  // Find greatest and smallest channel values
+  let cmin = Math.min(r,g,b),
+      cmax = Math.max(r,g,b),
+      delta = cmax - cmin,
+      h = 0,
+      s = 0,
+      l = 0;
+
+  // Calculate hue
+  // No difference
+  if (delta == 0)
+    h = 0;
+  // Red is max
+  else if (cmax == r)
+    h = ((g - b) / delta) % 6;
+  // Green is max
+  else if (cmax == g)
+    h = (b - r) / delta + 2;
+  // Blue is max
+  else
+    h = (r - g) / delta + 4;
+
+  h = Math.round(h * 60);
+
+  // Make negative hues positive behind 360°
+  if (h < 0)
+      h += 360;
+
+  // Calculate lightness
+  l = (cmax + cmin) / 2;
+
+  // Calculate saturation
+  s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+  // Multiply l and s by 100
+  s = +(s * 100).toFixed(1);
+  l = +(l * 100).toFixed(1);
+
+  return "hsl(" + h + "," + s + "%," + l + "%)";
 }
