e09c64ab3832eeb79dcb9254d1f5922681cbf38c
Jess <jessicatanhc@gmail.com>
2020-01-09 17:38:57 +0800

    Add HSL

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ var convertType = process.argv[2];
 if(convertType === "hex")
 {
     var hexValue = process.argv[3];
+
     console.log(convertHex.hexToRgb(hexValue));
+    if(convertHex.hexToRgb(hexValue) !== null){
+     console.log(convertHex.hexToHSL(hexValue));
+    }
 }else if(convertType === "rgb"){
    var r = parseInt(process.argv[3]);
    var g = parseInt(process.argv[4]);
    var b = parseInt(process.argv[5]);
+
     console.log(convertRgb.rgbToHex(r,g,b));
+    console.log(convertRgb.RGBToHSL(r,g,b));
 }
